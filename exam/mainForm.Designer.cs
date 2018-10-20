063065c994d8c30fe6d84cb509fb40681477ec27
squallhanxi <hxbyte@163.com>
2018-10-20 21:59:31 +0800

    Initial commit

---
+++ exam/mainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace exam
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.paperid_textBox = new System.Windows.Forms.TextBox();
+            this.pid_label = new System.Windows.Forms.Label();
+            this.answer_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.main_webBrowser = new System.Windows.Forms.WebBrowser();
+            this.hide_webBrowser = new System.Windows.Forms.WebBrowser();
+            this.url_textBox = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            this.getanswer_button = new System.Windows.Forms.Button();
+            this.postanswer_button = new System.Windows.Forms.Button();
+            this.url_label = new System.Windows.Forms.Label();
+            this.new_radioButton = new System.Windows.Forms.RadioButton();
+            this.continue_radioButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // paperid_textBox
+            // 
+            this.paperid_textBox.Location = new System.Drawing.Point(71, 52);
+            this.paperid_textBox.Name = "paperid_textBox";
+            this.paperid_textBox.Size = new System.Drawing.Size(134, 21);
+            this.paperid_textBox.TabIndex = 0;
+            // 
+            // pid_label
+            // 
+            this.pid_label.AutoSize = true;
+            this.pid_label.Location = new System.Drawing.Point(12, 55);
+            this.pid_label.Name = "pid_label";
+            this.pid_label.Size = new System.Drawing.Size(53, 12);
+            this.pid_label.TabIndex = 1;
+            this.pid_label.Text = "PaperID:";
+            // 
+            // answer_richTextBox
+            // 
+            this.answer_richTextBox.Location = new System.Drawing.Point(345, 12);
+            this.answer_richTextBox.Name = "answer_richTextBox";
+            this.answer_richTextBox.Size = new System.Drawing.Size(460, 61);
+            this.answer_richTextBox.TabIndex = 3;
+            this.answer_richTextBox.Text = "";
+            // 
+            // main_webBrowser
+            // 
+            this.main_webBrowser.Location = new System.Drawing.Point(12, 115);
+            this.main_webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.main_webBrowser.Name = "main_webBrowser";
+            this.main_webBrowser.ScriptErrorsSuppressed = true;
+            this.main_webBrowser.Size = new System.Drawing.Size(793, 481);
+            this.main_webBrowser.TabIndex = 5;
+            this.main_webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.main_webBrowser_DocumentCompleted);
+            this.main_webBrowser.NewWindow += new System.ComponentModel.CancelEventHandler(this.main_webBrowser_NewWindow);
+            // 
+            // hide_webBrowser
+            // 
+            this.hide_webBrowser.Location = new System.Drawing.Point(735, 235);
+            this.hide_webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.hide_webBrowser.Name = "hide_webBrowser";
+            this.hide_webBrowser.ScriptErrorsSuppressed = true;
+            this.hide_webBrowser.Size = new System.Drawing.Size(46, 39);
+            this.hide_webBrowser.TabIndex = 6;
+            this.hide_webBrowser.Visible = false;
+            this.hide_webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.hide_webBrowser_DocumentCompleted);
+            // 
+            // url_textBox
+            // 
+            this.url_textBox.Location = new System.Drawing.Point(47, 88);
+            this.url_textBox.Name = "url_textBox";
+            this.url_textBox.Size = new System.Drawing.Size(758, 21);
+            this.url_textBox.TabIndex = 7;
+            // 
+            // login_button
+            // 
+            this.login_button.Location = new System.Drawing.Point(12, 12);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(101, 30);
+            this.login_button.TabIndex = 8;
+            this.login_button.Text = "登录考试";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // getanswer_button
+            // 
+            this.getanswer_button.Location = new System.Drawing.Point(119, 12);
+            this.getanswer_button.Name = "getanswer_button";
+            this.getanswer_button.Size = new System.Drawing.Size(101, 30);
+            this.getanswer_button.TabIndex = 9;
+            this.getanswer_button.Text = "抓取答案";
+            this.getanswer_button.UseVisualStyleBackColor = true;
+            this.getanswer_button.Click += new System.EventHandler(this.getanswer_button_Click);
+            // 
+            // postanswer_button
+            // 
+            this.postanswer_button.Location = new System.Drawing.Point(226, 12);
+            this.postanswer_button.Name = "postanswer_button";
+            this.postanswer_button.Size = new System.Drawing.Size(101, 30);
+            this.postanswer_button.TabIndex = 10;
+            this.postanswer_button.Text = "投递答案";
+            this.postanswer_button.UseVisualStyleBackColor = true;
+            this.postanswer_button.Click += new System.EventHandler(this.postanswer_button_Click);
+            // 
+            // url_label
+            // 
+            this.url_label.AutoSize = true;
+            this.url_label.Location = new System.Drawing.Point(12, 91);
+            this.url_label.Name = "url_label";
+            this.url_label.Size = new System.Drawing.Size(29, 12);
+            this.url_label.TabIndex = 11;
+            this.url_label.Text = "URL:";
+            // 
+            // new_radioButton
+            // 
+            this.new_radioButton.AutoSize = true;
+            this.new_radioButton.Checked = true;
+            this.new_radioButton.Location = new System.Drawing.Point(226, 54);
+            this.new_radioButton.Name = "new_radioButton";
+            this.new_radioButton.Size = new System.Drawing.Size(47, 16);
+            this.new_radioButton.TabIndex = 12;
+            this.new_radioButton.TabStop = true;
+            this.new_radioButton.Text = "新考";
+            this.new_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // continue_radioButton
+            // 
+            this.continue_radioButton.AutoSize = true;
+            this.continue_radioButton.Location = new System.Drawing.Point(277, 54);
+            this.continue_radioButton.Name = "continue_radioButton";
+            this.continue_radioButton.Size = new System.Drawing.Size(47, 16);
+            this.continue_radioButton.TabIndex = 13;
+            this.continue_radioButton.TabStop = true;
+            this.continue_radioButton.Text = "续考";
+            this.continue_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 608);
+            this.Controls.Add(this.continue_radioButton);
+            this.Controls.Add(this.new_radioButton);
+            this.Controls.Add(this.url_label);
+            this.Controls.Add(this.postanswer_button);
+            this.Controls.Add(this.getanswer_button);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.url_textBox);
+            this.Controls.Add(this.hide_webBrowser);
+            this.Controls.Add(this.main_webBrowser);
+            this.Controls.Add(this.answer_richTextBox);
+            this.Controls.Add(this.pid_label);
+            this.Controls.Add(this.paperid_textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动考试";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox paperid_textBox;
+        private System.Windows.Forms.Label pid_label;
+        private System.Windows.Forms.RichTextBox answer_richTextBox;
+        private System.Windows.Forms.WebBrowser main_webBrowser;
+        private System.Windows.Forms.WebBrowser hide_webBrowser;
+        private System.Windows.Forms.TextBox url_textBox;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Button getanswer_button;
+        private System.Windows.Forms.Button postanswer_button;
+        private System.Windows.Forms.Label url_label;
+        private System.Windows.Forms.RadioButton new_radioButton;
+        private System.Windows.Forms.RadioButton continue_radioButton;
+    }
+}
+
---
+++ exam/mainForm.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace exam
+{
+    public partial class mainForm : Form
+    {
+        //登录页面地址
+        string login_url = "http://www.jinmei.com";
+        
+        //答案页面地址前部分
+        string std_answer_a = "http://www.jinmei.com:8099/Examination/AnswerPaper.aspx?PaperId=";
+        
+        //答案页面地址后部分
+        string std_answer_b = "&Type=813B1687B1EE30B91210D2567F4283FB&examid=3535";
+
+        //考试页面地址变量，检测到该地址时直接打开，去除框架
+        string exam_sys;
+        string my_exam;
+
+        //答案数组
+        string[] an = new string[45];
+
+        //临时变量
+        string tmp;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        //登录按钮事件
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            main_webBrowser.Navigate(login_url);
+
+            if (continue_radioButton.Checked == true)
+            {
+                exam_sys = "http://www.jinmei.com:8099/Interface/index.aspx";
+                my_exam = "http://www.jinmei.com:8099/Examination/MyExam.aspx";
+            }
+            else
+            {
+                exam_sys = "http://www.jinmei.com:8099/Interface/index.aspx";
+                my_exam = "http://www.jinmei.com:8099/Examination/ExamCustom.aspx";
+            }
+        }
+                
+        private void main_webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //每次更换页面，向url_textBox填入当前url
+            url_textBox.Text = main_webBrowser.Url.ToString();
+
+            //当点击自主考试时，强制去除框架
+            if (main_webBrowser.Url.ToString() == exam_sys)
+            {
+                main_webBrowser.Navigate(my_exam);
+            }
+
+            //寻找PaperId
+            if (url_textBox.Text.IndexOf("PaperId") != -1)
+            {
+                paperid_textBox.Text = url_textBox.Text.Substring(url_textBox.Text.IndexOf("PaperId") + 8 ,16);
+            }
+        }
+
+        //强制在当前窗口打开页面
+        private void main_webBrowser_NewWindow(object sender, CancelEventArgs e)
+        {
+            WebBrowser wbt = (WebBrowser)sender;
+            main_webBrowser.Navigate(wbt.Document.ActiveElement.GetAttribute("href"));
+            e.Cancel = true;
+        }
+
+        //答案抓取
+        private void getanswer_button_Click(object sender, EventArgs e)
+        {
+            string answer_url_all = std_answer_a + paperid_textBox.Text + std_answer_b;
+            hide_webBrowser.Navigate(answer_url_all);
+        }
+
+        //以下执行一系列答案抓取代码，格式化后填入an数组，并显示在answer_richTextBox中
+        private void hide_webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            an[0] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl00_lblStandardAnswer").InnerText;
+            an[1] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl01_lblStandardAnswer").InnerText;
+            an[2] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl02_lblStandardAnswer").InnerText;
+            an[3] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl03_lblStandardAnswer").InnerText;
+            an[4] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl04_lblStandardAnswer").InnerText;
+            an[5] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl05_lblStandardAnswer").InnerText;
+            an[6] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl06_lblStandardAnswer").InnerText;
+            an[7] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl07_lblStandardAnswer").InnerText;
+            an[8] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl08_lblStandardAnswer").InnerText;
+            an[9] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl09_lblStandardAnswer").InnerText;
+
+            an[10] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl10_lblStandardAnswer").InnerText;
+            an[11] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl11_lblStandardAnswer").InnerText;
+            an[12] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl12_lblStandardAnswer").InnerText;
+            an[13] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl13_lblStandardAnswer").InnerText;
+            an[14] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl14_lblStandardAnswer").InnerText;
+            an[15] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl15_lblStandardAnswer").InnerText;
+            an[16] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl16_lblStandardAnswer").InnerText;
+            an[17] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl17_lblStandardAnswer").InnerText;
+            an[18] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl18_lblStandardAnswer").InnerText;
+            an[19] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl19_lblStandardAnswer").InnerText;
+
+            an[20] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl20_lblStandardAnswer").InnerText;
+            an[21] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl21_lblStandardAnswer").InnerText;
+            an[22] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl22_lblStandardAnswer").InnerText;
+            an[23] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl23_lblStandardAnswer").InnerText;
+            an[24] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl24_lblStandardAnswer").InnerText;
+            an[25] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl25_lblStandardAnswer").InnerText;
+            an[26] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl26_lblStandardAnswer").InnerText;
+            an[27] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl27_lblStandardAnswer").InnerText;
+            an[28] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl28_lblStandardAnswer").InnerText;
+            an[29] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl29_lblStandardAnswer").InnerText;
+
+            an[30] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl30_lblStandardAnswer").InnerText;
+            an[31] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl31_lblStandardAnswer").InnerText;
+            an[32] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl32_lblStandardAnswer").InnerText;
+            an[33] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl33_lblStandardAnswer").InnerText;
+            an[34] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl34_lblStandardAnswer").InnerText;
+            an[35] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl35_lblStandardAnswer").InnerText;
+            an[36] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl36_lblStandardAnswer").InnerText;
+            an[37] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl37_lblStandardAnswer").InnerText;
+            an[38] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl38_lblStandardAnswer").InnerText;
+            an[39] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl39_lblStandardAnswer").InnerText;
+            an[40] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl40_lblStandardAnswer").InnerText;
+            an[41] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl41_lblStandardAnswer").InnerText;
+            an[42] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl42_lblStandardAnswer").InnerText;
+            an[43] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl43_lblStandardAnswer").InnerText;
+            an[44] = hide_webBrowser.Document.GetElementById("dlQuestions_ctl44_lblStandardAnswer").InnerText;
+
+            for (int i = 0; i < 45; i++)
+            {
+                answer_richTextBox.AppendText(an[i] + "\r\n");
+            }
+            
+        }
+
+        //将已抓取的答案投递到考试页面
+        private void postanswer_button_Click(object sender, EventArgs e)
+        {
+            //投递单选题
+            for (int i = 0; i < 10; i++ )
+            {
+                switch (an[i].Substring(0,1))
+                {
+                    case "A":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl0" + i.ToString() + "_rbSingleOptions_0").SetAttribute("checked", "true");
+                        break;
+                    case "B":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl0" + i.ToString() + "_rbSingleOptions_1").SetAttribute("checked", "true");
+                        break;
+                    case "C":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl0" + i.ToString() + "_rbSingleOptions_2").SetAttribute("checked", "true");
+                        break;
+                    case "D":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl0" + i.ToString() + "_rbSingleOptions_3").SetAttribute("checked", "true");
+                        break;
+                }
+            }
+
+            //投递多选题
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < an[i + 10].Length;j++ )
+                {
+                    if (j == 0)
+                    {
+                        switch (an[i + 10].Substring(0, 1))
+                        {
+                            case "A":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_0").SetAttribute("checked", "checked");
+                                break;
+                            case "B":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_1").SetAttribute("checked", "checked");
+                                break;
+                            case "C":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_2").SetAttribute("checked", "checked");
+                                break;
+                            case "D":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_3").SetAttribute("checked", "checked");
+                                break;
+                        }
+                    }
+
+                    if (an[i + 10].Substring(j, 1) == " " && j != 0 && j != an[i + 10].Length - 1)
+                    {
+                        switch (an[i+10].Substring((j+1), 1))
+                        {
+                            case "A":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_0").SetAttribute("checked", "checked");
+                                break;
+                            case "B":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_1").SetAttribute("checked", "checked");
+                                break;
+                            case "C":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_2").SetAttribute("checked", "checked");
+                                break;
+                            case "D":
+                                main_webBrowser.Document.GetElementById("dlQuestions_ctl1" + i.ToString() + "_cbMultiOptions_3").SetAttribute("checked", "checked");
+                                break;
+                        }
+                    }
+                }
+            }
+
+            //投递填空题
+            for (int i = 0; i < 10; i++)
+            {
+                int k = 0;
+                for (int j = 0; j < an[i + 20].Length; j++)
+                {
+                    if (an[i + 20].Substring(j, 1) != " ")
+                    {
+                        tmp = tmp + an[i + 20].Substring(j, 1);
+                    }
+                    else
+                    {
+                        k++;
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl2" + i.ToString() + "_TextBox" + k.ToString()).SetAttribute("value", tmp);
+                        tmp = "";
+                    }
+                }
+            }
+
+            //投递判断题
+            for (int i = 0; i < 10; i++)
+            {
+                switch (an[i + 30].Substring(0, 1))
+                {
+                    case "A":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl3" + i.ToString() + "_rbSingleOptions_0").SetAttribute("checked", "true");
+                        break;
+                    case "B":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl3" + i.ToString() + "_rbSingleOptions_1").SetAttribute("checked", "true");
+                        break;
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                switch (an[i + 40].Substring(0, 1))
+                {
+                    case "A":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl4" + i.ToString() + "_rbSingleOptions_0").SetAttribute("checked", "true");
+                        break;
+                    case "B":
+                        main_webBrowser.Document.GetElementById("dlQuestions_ctl4" + i.ToString() + "_rbSingleOptions_1").SetAttribute("checked", "true");
+                        break;
+                }
+            }
+
+            MessageBox.Show("投递完成，请3分钟后提交试卷。");
+        }
+    }
+}
